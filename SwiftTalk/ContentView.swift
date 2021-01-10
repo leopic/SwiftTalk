f320667f94c9d79bc3c1e1d612e27b3f1c0ecf62
Leo Picado <leopic@gmail.com>
2021-01-09 18:00:55 -0600

    Initial Commit

---
+++ SwiftTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftTalk
+//
+//  Created by Leo Picado on 1/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftTalk/SwiftTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTalkApp.swift
+//  SwiftTalk
+//
+//  Created by Leo Picado on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
